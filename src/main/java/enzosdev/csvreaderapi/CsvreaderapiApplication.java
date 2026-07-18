7f9dbaa671c07f11a9c37316d8d4e1363720cc3b
enzosdev <101185305+Enzosantos04@users.noreply.github.com>
2026-07-18 19:03:58 -0300

    feat: initialize project structure

---
+++ src/main/java/enzosdev/csvreaderapi/CsvreaderapiApplication.java
@@ -0,0 +1,13 @@
+package enzosdev.csvreaderapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsvreaderapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CsvreaderapiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/enzosdev/csvreaderapi/CsvreaderapiApplicationTests.java
@@ -0,0 +1,13 @@
+package enzosdev.csvreaderapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsvreaderapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
