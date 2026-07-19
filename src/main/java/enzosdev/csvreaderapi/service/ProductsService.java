40989740baf01831615ed6311e2c807510f80a60
enzosdev <101185305+Enzosantos04@users.noreply.github.com>
2026-07-18 23:38:22 -0300

    feat: add ProductService with findAll products method

--- src/main/java/enzosdev/csvreaderapi/service/ProductsService.java
+++ src/main/java/enzosdev/csvreaderapi/service/ProductsService.java
@@ -1,4 +1,27 @@
 package enzosdev.csvreaderapi.service;
 
+import enzosdev.csvreaderapi.dto.ProductsResponse;
+import enzosdev.csvreaderapi.mapper.ProductsMapper;
+import enzosdev.csvreaderapi.repository.ProductsRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+
+@Service
 public class ProductsService {
+
+    private final ProductsRepository productsRepository;
+    private final ProductsMapper productsMapper;
+
+    public ProductsService(ProductsRepository productsRepository, ProductsMapper productsMapper) {
+        this.productsRepository = productsRepository;
+        this.productsMapper = productsMapper;
+    }
+
+
+    public Page<ProductsResponse> findAll(Pageable pageable) {
+        return productsRepository.findAll(pageable)
+                .map(productsMapper::toResponse);
+    }
 }
