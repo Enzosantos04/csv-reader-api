42c928cb40bca9e5b5e08de3bb66d4ec524be7c9
enzosdev <101185305+Enzosantos04@users.noreply.github.com>
2026-07-18 23:33:12 -0300

    feat: add ProductsDTO with CsvBindByName to map csv columns

--- src/main/java/enzosdev/csvreaderapi/dto/ProductsDTO.java
+++ src/main/java/enzosdev/csvreaderapi/dto/ProductsDTO.java
@@ -1,4 +1,25 @@
 package enzosdev.csvreaderapi.dto;
 
+import com.opencsv.bean.CsvBindByName;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ProductsDTO {
+
+
+    private Long id;
+    @CsvBindByName(column = "nome do produto")
+    private String name;
+
+    @CsvBindByName(column = "preco")
+    private BigDecimal price;
+
+    @CsvBindByName(column = "quantidade")
+    private int quantityStock;
 }
