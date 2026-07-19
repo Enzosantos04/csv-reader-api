09df1d8477faaff4b8f4e8032214f50b6b5a848f
enzosdev <101185305+Enzosantos04@users.noreply.github.com>
2026-07-18 23:35:17 -0300

    feat: add ProductsMapper to convert Dto to entity and entity to response

--- src/main/java/enzosdev/csvreaderapi/mapper/ProductsMapper.java
+++ src/main/java/enzosdev/csvreaderapi/mapper/ProductsMapper.java
@@ -1,4 +1,27 @@
 package enzosdev.csvreaderapi.mapper;
 
+import enzosdev.csvreaderapi.dto.ProductsDTO;
+import enzosdev.csvreaderapi.dto.ProductsResponse;
+import enzosdev.csvreaderapi.entity.Products;
+import org.springframework.stereotype.Component;
+
+@Component
 public class ProductsMapper {
+
+    public Products toEntity(ProductsDTO dto) {
+        Products products = new Products();
+        products.setName(dto.getName());
+        products.setPrice(dto.getPrice());
+        products.setQuantityStock(dto.getQuantityStock());
+        return products;
+    }
+
+
+    public ProductsResponse toResponse(Products products) {
+        ProductsResponse productsResponse = new ProductsResponse();
+        productsResponse.setName(products.getName());
+        productsResponse.setPrice(products.getPrice());
+        productsResponse.setQuantityStock(products.getQuantityStock());
+        return productsResponse;
+    }
 }
