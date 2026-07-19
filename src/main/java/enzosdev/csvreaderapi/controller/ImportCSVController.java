031b8d4251d67b05225ae515c1c2edd42ff264f4
enzosdev <101185305+Enzosantos04@users.noreply.github.com>
2026-07-18 23:40:04 -0300

    feat: add ImportCsvController with ImportCSV endpoint

--- src/main/java/enzosdev/csvreaderapi/controller/ImportCSVController.java
+++ src/main/java/enzosdev/csvreaderapi/controller/ImportCSVController.java
@@ -1,6 +1,6 @@
 package enzosdev.csvreaderapi.controller;
 
-import enzosdev.csvreaderapi.service.CsvProductsImport;
+import enzosdev.csvreaderapi.service.CsvProductsImportService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,15 +8,13 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
-
 @RequestMapping("/import-csv")
 @RestController
-public class ImportCSV {
+public class ImportCSVController {
 
-    private final CsvProductsImport csvProductsImport;
+    private final CsvProductsImportService csvProductsImport;
 
-    public ImportCSV(CsvProductsImport csvProductsImport) {
+    public ImportCSVController(CsvProductsImportService csvProductsImport) {
         this.csvProductsImport = csvProductsImport;
     }
 
