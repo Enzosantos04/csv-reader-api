b5051332206c69bd7ae04f8e66fca968f1f60855
enzosdev <101185305+Enzosantos04@users.noreply.github.com>
2026-07-18 23:34:12 -0300

    feat: add Products entity

--- src/main/java/enzosdev/csvreaderapi/entity/Products.java
+++ src/main/java/enzosdev/csvreaderapi/entity/Products.java
@@ -1,4 +1,24 @@
 package enzosdev.csvreaderapi.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "products")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Products {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private BigDecimal price;
+    private int quantityStock;
 }
