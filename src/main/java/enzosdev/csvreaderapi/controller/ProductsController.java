cb6419cd2bd952ccf43bba8d6012e2e84740f191
enzosdev <101185305+Enzosantos04@users.noreply.github.com>
2026-07-18 23:37:39 -0300

    feat add findAll products endpoint

--- src/main/java/enzosdev/csvreaderapi/controller/ProductsController.java
+++ src/main/java/enzosdev/csvreaderapi/controller/ProductsController.java
@@ -1,4 +1,33 @@
 package enzosdev.csvreaderapi.controller;
 
+
+import enzosdev.csvreaderapi.dto.ProductsResponse;
+import enzosdev.csvreaderapi.entity.Products;
+import enzosdev.csvreaderapi.service.ProductsService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RequestMapping
+@RestController
 public class ProductsController {
+
+    private final ProductsService productsService;
+
+    public ProductsController(ProductsService productsService) {
+        this.productsService = productsService;
+    }
+
+
+    @GetMapping("/products")
+    public ResponseEntity<Page<ProductsResponse>> findAll(Pageable pageable) {
+        Page<ProductsResponse> products = productsService.findAll(pageable);
+        return ResponseEntity.ok(products);
+
+    }
 }
