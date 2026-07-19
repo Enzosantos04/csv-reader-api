aba3a6b0fedef2473e30f6c87d10142b9a97374d
enzosdev <101185305+Enzosantos04@users.noreply.github.com>
2026-07-18 23:33:53 -0300

    feat: add ProductResponse dto

--- src/main/java/enzosdev/csvreaderapi/dto/ProductsResponse.java
+++ src/main/java/enzosdev/csvreaderapi/dto/ProductsResponse.java
@@ -1,4 +1,16 @@
 package enzosdev.csvreaderapi.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ProductsResponse {
+    private String name;
+    private BigDecimal price;
+    private int quantityStock;
 }
