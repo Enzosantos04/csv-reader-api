854811d70918034f54c8897f9fdc1ec9be5e168b
enzosdev <enzohfs@gmail.com>
2026-07-20 15:14:30 -0300

    feat: add findProductById endpoint

--- src/main/java/enzosdev/csvreaderapi/controller/ProductsController.java
+++ src/main/java/enzosdev/csvreaderapi/controller/ProductsController.java
@@ -5,12 +5,11 @@ import enzosdev.csvreaderapi.dto.ProductsResponse;
 import enzosdev.csvreaderapi.service.ProductsService;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-@RequestMapping
+@RequestMapping("/products")
 @RestController
 public class ProductsController {
 
@@ -25,6 +24,13 @@ public class ProductsController {
     public ResponseEntity<Page<ProductsResponse>> findAll(Pageable pageable) {
         Page<ProductsResponse> products = productsService.findAll(pageable);
         return ResponseEntity.ok(products);
+
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ProductsResponse> findProductById(@PathVariable Long id){
+        ProductsResponse products = productsService.findProductById(id);
+        return ResponseEntity.status(HttpStatus.OK).body(products);
 
     }
 }
