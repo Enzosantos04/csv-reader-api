ae8007c69829d718db401f0f3d5fffac638dd8c2
enzosdev <enzohfs@gmail.com>
2026-07-20 15:14:05 -0300

    feat: add findProductById method as Business rules

--- src/main/java/enzosdev/csvreaderapi/service/ProductsService.java
+++ src/main/java/enzosdev/csvreaderapi/service/ProductsService.java
@@ -1,11 +1,15 @@
 package enzosdev.csvreaderapi.service;
 
+import enzosdev.csvreaderapi.dto.ProductsDTO;
 import enzosdev.csvreaderapi.dto.ProductsResponse;
+import enzosdev.csvreaderapi.entity.Products;
 import enzosdev.csvreaderapi.mapper.ProductsMapper;
 import enzosdev.csvreaderapi.repository.ProductsRepository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 
 @Service
@@ -23,5 +27,13 @@ public class ProductsService {
     public Page<ProductsResponse> findAll(Pageable pageable) {
         return productsRepository.findAll(pageable)
                 .map(productsMapper::toResponse);
+    }
+
+    public ProductsResponse findProductById(Long id){
+        Optional<Products> products = productsRepository.findById(id);
+        return products.map(productsMapper::toResponse)
+                .orElseThrow(()->  new RuntimeException("erro"));
+
+
     }
 }
